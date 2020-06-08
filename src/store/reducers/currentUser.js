ab6c42da180393eeaf02b637b40fc16c64e61791
Pancho Fernandez <panchofern@gmail.com>
2020-06-07 20:42:32 -0400

    fixed reducer

--- src/store/reducers/currentUser.js
+++ src/store/reducers/currentUser.js
@@ -1,18 +1,21 @@
 const initialState ={
-	token:null
+	isAuthenticated:false,
+	user:{},
+	portfolio:null,
+	notifications:[]
 }
 
 const currentUser = (state=initialState, action)=>{
 	switch(action.type){
-		case "SET_CURRENT_USER":
+		case 'SET_CURRENT_USER':
 			return {
 				isAuthenticated:Object.keys(action.user).length>0,
-				user:action.user
-			}
-		case "FETCH_NOTIFICATIONS":
-			return {...state, notifications:action.notifications};
-		case 'LOGOUT':
-			return initialState;
+			 	user:action.user
+			};
+		case 'SET_USER_PORTFOLIO':
+			return {...state, portfolio:action.portfolio};
+		case 'FETCH_NOTIFICATIONS':
+			return {...state, notifications:[...action.notifications]};
 		default:
 			return state;
 	}
